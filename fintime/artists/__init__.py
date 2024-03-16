557c0e98cee3a5f740613958f4599b84d536299a
marcel-dehaan <mdehaan35@gmail.com>
2024-03-16 21:57:48 +0100

    fix: add DivergingBar to artists namespace

--- fintime/artists/__init__.py
+++ fintime/artists/__init__.py
@@ -3,5 +3,13 @@ from fintime.artists.candlestick import CandleStick
 from fintime.artists.line import Line
 from fintime.artists.volume import Volume
 from fintime.artists.fill_between import FillBetween
+from fintime.artists.diverging_bar import DivergingBar
 
-__all__ = ("CandleStick", "Volume", "Line", "Artist", "FillBetween")
+__all__ = (
+    "CandleStick",
+    "Volume",
+    "Line",
+    "Artist",
+    "FillBetween",
+    "DivergingBar",
+)
